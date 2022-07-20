86639e136e224f72e7e73f198679723148d3c277
frontend <gabriellennon@wizsolucoes.com.br>
2022-07-20 11:16:19 -0300

    component avatar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -9,7 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1635830625698-3b9bd74671ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/gabriellennon.png" />
+                <Avatar src="https://github.com/gabriellennon.png" />
                 <strong>Gabriel Lennon</strong>
                 <span>Web Developer</span>
             </div>
