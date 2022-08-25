3924a2b9be9045444f6d9ca151bbff7263de41a2
frontend <gabriellennon@wizsolucoes.com.br>
2022-08-25 00:10:40 -0300

    format date and content map

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gabriellennon.png',
+      name: 'Gabriel Lennon',
+      role: 'Software Engineer @ iFood'
+    },
+    content : [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishAt: new Date('2022-08-24 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    },
+    content : [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishAt: new Date('2022-08-23 22:00:00'),
+  },
+];
+
 function App() {
 
   return (
@@ -15,8 +46,19 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {
+            //Usamos o map ao invés do forEach, pois o map retorna algo e o forEach não
+            posts.map(post => {
+              return (
+                <Post 
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  published={post.publishAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </div>
