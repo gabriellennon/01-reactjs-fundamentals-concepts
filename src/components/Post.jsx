781386e3f31a629ac8ba27c79c600680d74a98f5
frontend <gabriellennon@wizsolucoes.com.br>
2022-10-10 14:05:51 -0300

    delete comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,6 +31,14 @@ export function Post({ author, published, content }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeteledOne = comments.filter(comment => {
+            return comment !== commentToDelete
+        })
+
+        setComments(commentsWithoutDeteledOne);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -50,9 +58,9 @@ export function Post({ author, published, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type === 'paragraph') {
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     } else if (line.type === 'link') {
-                        return <p><a href='#'>{line.content}</a></p>
+                        return <p key={line.content}><a href='#'>{line.content}</a></p>
                     }
                 })}
             </div>
@@ -73,7 +81,7 @@ export function Post({ author, published, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment}/>
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
                 })}
             </div>
         </article>
